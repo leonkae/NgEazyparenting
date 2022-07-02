d391d867cdac0ad8dc7e6991d67c2c08094b8c81
KamauDev-maker <oscarnjenga@gmail.com>
2022-07-02 10:11:44 +0300

    consuming the api

--- src/app/parenting.service.ts
+++ src/app/parenting.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders, HttpErrorResponse} from '@angular/common/http';
+import { Observable,throwError} from 'rxjs';
+import{map} from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
+const endpoint = 'https://africa-emergency-contacts.herokuapp.com/all/';
 export class ParentingService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+    private handleError(error: HttpErrorResponse): any {
+      if (error.error instanceof ErrorEvent) {
+        console.error('An error occurred:', error.error.message);
+      } else {
+        console.error(
+          `Backend returned code ${error.status}, ` +
+          `body was: ${error.error}`);
+      }
+      return throwError(
+        'Something bad happened; please try again later.');
+    }
+  }
 }
