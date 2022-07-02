75f00c406e734b61f98f4ddcbcff253dd81de50d
KamauDev-maker <oscarnjenga@gmail.com>
2022-07-02 13:32:35 +0300

    adding a method to get country data

--- src/app/parenting.service.ts
+++ src/app/parenting.service.ts
@@ -1,26 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,HttpHeaders, HttpErrorResponse} from '@angular/common/http';
-import { Observable,throwError} from 'rxjs';
-import{map} from 'rxjs/operators';
+import { HttpClient} from '@angular/common/http';
 
 
 @Injectable({
   providedIn: 'root'
 })
-const endpoint = 'https://africa-emergency-contacts.herokuapp.com/all/';
 export class ParentingService {
 
-  constructor(private http: HttpClient) { 
-    private handleError(error: HttpErrorResponse): any {
-      if (error.error instanceof ErrorEvent) {
-        console.error('An error occurred:', error.error.message);
-      } else {
-        console.error(
-          `Backend returned code ${error.status}, ` +
-          `body was: ${error.error}`);
-      }
-      return throwError(
-        'Something bad happened; please try again later.');
-    }
+  constructor(private httpClient: HttpClient) { }
+
+  public getCountry(){
+    return this.httpClient.get('https://africa-emergency-contacts.herokuapp.com/all/');
   }
+
 }
