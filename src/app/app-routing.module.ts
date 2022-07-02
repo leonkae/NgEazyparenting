0817f13173aa83517d8fe3d0c65b6c088d46d188
leonkae <leon29kae@gmail.com>
2022-07-02 17:34:08 +0300

    components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfilesComponent } from './profiles/profiles.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'profiles/', component: ProfilesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfilesComponent } from './profiles/profiles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfilesComponent,
+
   ],
   imports: [
     BrowserModule,
