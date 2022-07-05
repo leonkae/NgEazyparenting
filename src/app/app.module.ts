fb77ed9ae1c3d54104875a659b82cbbe20419b4f
Shalyne-Waweru <shalynewaweru@gmail.com>
2022-07-05 19:40:43 +0300

    Add and style the navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfilesComponent } from './profiles/profiles.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfilesComponent,
+    NavbarComponent,
 
   ],
   imports: [
