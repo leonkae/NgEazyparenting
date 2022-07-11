2536b7e6711b5254c0b7676575e845b3cf68c31c
Shalyne-Waweru <shalynewaweru@gmail.com>
2022-07-11 11:36:49 +0300

    Solve conflicts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfilesComponent } from './profiles/profiles.component';
-import { ContactsComponent } from './contacts/contacts.component';
 import { MedicalHubComponent } from './medical-hub/medical-hub.component';
 
 const routes: Routes = [
   { path: 'profiles', component: ProfilesComponent },
-  { path: 'contacts', component: ContactsComponent},
   { path: 'explorer', component: MedicalHubComponent },
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ContactsComponent } from './contacts/contacts.component';
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ProfilesComponent } from './profiles/profiles.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MedicalHubComponent } from './medical-hub/medical-hub.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactsComponent,
     ProfilesComponent,
     NavbarComponent,
     MedicalHubComponent,
@@ -28,21 +21,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-<<<<<<< HEAD
-    FormsModule,
     ReactiveFormsModule,
-    MatInputModule,
-    MatAutocompleteModule,
-    BrowserAnimationsModule,
-    
-
-  
-
-    AppRoutingModule
-=======
     NgbModule,
-    ReactiveFormsModule
->>>>>>> 7518485a9c069927d99711ee55ad2884aff1e3a4
   ],
   providers: [],
   bootstrap: [AppComponent]
