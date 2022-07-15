ea53aaaf6cad82d3c2208861ff249e4523197891
Shalyne-Waweru <shalynewaweru@gmail.com>
2022-07-15 09:56:31 +0300

    Add deployment changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  REG_URL: 'http://localhost:8000/accounts/login/',
+  SIGN_URL: 'http://localhost:8000/accounts/registration/',
+  API_TOKEN: 'http://localhost:8000/api/token/',
+  PROF_URL: 'http://localhost:8000/accounts/profile',
+  DEV_URL: 'http://localhost:8000/accounts/profile/1',
+  API_URL: 'http://localhost:8000/api',
+  TASKS_URL:' http://127.0.0.1:8000/api/tasks/',
+  ACCOUNTS_URL : 'http://localhost:8000/accounts',
+  MEDICALHUB: 'http://127.0.0.1:8000/api/explorer/',
+  COMMENT_URL: 'http://127.0.0.1:8000/api/comment/'
 };
